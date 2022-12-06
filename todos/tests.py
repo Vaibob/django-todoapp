5cb1089c887a6322d942fb4842bd06ba66017b04
Vaibhav Shelar <vaibhavshelar269@gmail.com>
2022-12-06 15:56:03 +0530

    test cases added in /todos/test.py

--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Todo
 
-# Create your tests here.
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        Todo.objects.create(title="Todo1", isCompleted=True)
+
+    def test1(self):
+        todo1 = Todo.objects.get(title="Todo1")
+        self.assertEqual(todo1.isCompleted, True)
